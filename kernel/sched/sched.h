283431635fdcd19ecce7a11b7abd22814d66d0c1
Francisco Franco <franciscofranco.1990@gmail.com>
2013-09-02 01:55:31 +0100

    Revert "sched_avg: add run queue averaging."
    We're not using this so lets save calls every time there is run_queue change.
    
    This reverts commit e163bf662abb0a69aa08ed72885d1fae23a5b8c2.

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1106,7 +1106,6 @@ static inline u64 steal_ticks(u64 steal)
 
 static inline void inc_nr_running(struct rq *rq)
 {
-	sched_update_nr_prod(cpu_of(rq), rq->nr_running, true);
 	rq->nr_running++;
 
 #ifdef CONFIG_NO_HZ_FULL
@@ -1122,7 +1121,6 @@ static inline void inc_nr_running(struct rq *rq)
 
 static inline void dec_nr_running(struct rq *rq)
 {
-	sched_update_nr_prod(cpu_of(rq), rq->nr_running, false);
 	rq->nr_running--;
 }
 
