4e9bce348d4017a8bc667755ac692c57650e4ebc
Laura Abbott <lauraa@codeaurora.org>
2014-02-25 09:46:47 -0800

    ion: Remove custom compat framework
    It was pointed out that there is no need for a custom compat ioctl
    framework in Ion, that decision can all be handled in device
    registration. Remove the custom compat framework and just handle
    the decision regarding compat ioctls in msm_ion.c
    
    Change-Id: I6a0791013ce8e1f2f679555a46afd33de8fd9ce6
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -63,9 +63,6 @@ struct ion_device {
 	struct plist_head heaps;
 	long (*custom_ioctl) (struct ion_client *client, unsigned int cmd,
 			      unsigned long arg);
-	long (*custom_compat_ioctl) (struct ion_client *client,
-			      unsigned int cmd,
-			      unsigned long arg);
 	struct rb_root clients;
 	struct dentry *debug_root;
 	struct dentry *heaps_debug_root;
@@ -1561,12 +1558,7 @@ static long ion_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	{
 		if (!dev->custom_ioctl)
 			return -ENOTTY;
-
-		if (dev->custom_compat_ioctl)
-			ret = dev->custom_compat_ioctl(client, data.custom.cmd,
-						data.custom.arg);
-		else
-			ret = dev->custom_ioctl(client, data.custom.cmd,
+		ret = dev->custom_ioctl(client, data.custom.cmd,
 						data.custom.arg);
 		break;
 	}
@@ -1939,10 +1931,6 @@ EXPORT_SYMBOL(ion_walk_heaps);
 struct ion_device *ion_device_create(long (*custom_ioctl)
 				     (struct ion_client *client,
 				      unsigned int cmd,
-				      unsigned long arg),
-				  long (*custom_compat_ioctl)
-				     (struct ion_client *client,
-				      unsigned int cmd,
 				      unsigned long arg))
 {
 	struct ion_device *idev;
@@ -1980,7 +1968,6 @@ struct ion_device *ion_device_create(long (*custom_ioctl)
 debugfs_done:
 
 	idev->custom_ioctl = custom_ioctl;
-	idev->custom_compat_ioctl = custom_compat_ioctl;
 	idev->buffers = RB_ROOT;
 	mutex_init(&idev->buffer_lock);
 	init_rwsem(&idev->lock);
