06ee392649f49bc76e6a91da2f90f8199063b3ec
Mitchel Humpherys <mitchelh@codeaurora.org>
2014-04-29 11:02:14 -0700

    ion: msm: remove superfluous function argument
    msm_ion_client_create doesn't actually do anything with its heap_mask
    parameter. Remove it. Also remove the extra argument from an audio
    function that wraps msm_ion_client_create.
    
    The following semantic patch was used to generate this patch:
    
        @@
        expression E1, E2;
        @@
    
        msm_ion_client_create(
        -       E1,
                E2)
    
        @@
        expression E1, E2;
        @@
    
        msm_audio_ion_client_create(
        -       E1,
                E2)
    
    Change-Id: I403a125a1715b29a3db1f27c993abe0bc6d3fb11
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1295,7 +1295,7 @@ static inline void __mdss_fb_set_page_protection(struct vm_area_struct *vma,
 
 static inline int mdss_fb_create_ion_client(struct msm_fb_data_type *mfd)
 {
-	mfd->fb_ion_client  = msm_ion_client_create(-1 , "mdss_fb_iclient");
+	mfd->fb_ion_client  = msm_ion_client_create("mdss_fb_iclient");
 	if (IS_ERR_OR_NULL(mfd->fb_ion_client)) {
 		pr_err("Err:client not created, val %d\n",
 				PTR_RET(mfd->fb_ion_client));
