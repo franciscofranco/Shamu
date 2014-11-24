1cb06f2fe37fb4abeb9082c1b8a9a49b6f0ae4e3
Praveen Chidambaram <pchidamb@codeaurora.org>
2014-11-24 10:13:20 -0700

    irq: smp_affinity: Initialize work struct only once
    The work function to handle the irq affinity change is currently being
    set from setup_affinity, which is called whenever the affinity changes.
    Initialize the work function only once when the irq desc object's
    defaults are set up.
    
    CRs-Fixed: 756463
    Change-Id: I66732f8c01cba166c41ce89c329d313eeaea8a7d
    Signed-off-by: Praveen Chidambaram <pchidamb@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -379,6 +379,7 @@ extern void irq_cpu_online(void);
 extern void irq_cpu_offline(void);
 extern int irq_set_affinity_locked(struct irq_data *data,
 				   const struct cpumask *cpumask, bool force);
+extern void irq_affinity_notify(struct work_struct *work);
 
 #ifdef CONFIG_GENERIC_HARDIRQS
 
