7515768da950b4f41e2ecb43eb3ff592316d9c61
Chintan Pandya <cpandya@codeaurora.org>
2014-12-17 12:51:41 +0530

    dma-contiguous: Re-order the error handling sequence
    When CMA allocation fails because of any pending
    signals, we clear off pfn and bit map before
    returning back. While clearing off the bitmap,
    we still use the pfn info so, clear pfn only
    after bitmap is cleared.
    
    CRs-fixed: 772299
    Change-Id: I94e566181f75b7c8ebdab7d29437e5fca5f3fbdc
    Signed-off-by: Chintan Pandya <cpandya@codeaurora.org>

--- drivers/base/dma-contiguous.c
+++ drivers/base/dma-contiguous.c
@@ -589,8 +589,8 @@ unsigned long dma_alloc_from_contiguous(struct device *dev, size_t count,
 		if (ret == 0) {
 			break;
 		} else if (ret != -EBUSY) {
-			pfn = 0;
 			clear_cma_bitmap(cma, pfn, count);
+			pfn = 0;
 			break;
 		}
 		clear_cma_bitmap(cma, pfn, count);
