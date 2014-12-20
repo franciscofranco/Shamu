d5ee2614763c7d097279795296ae2caccdc05b0c
imoseyon <imoseyon@gmail.com>
2014-12-19 20:34:56 -0800

    power: smb135x: bump up 300-400mA for PC/USB charging
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/power/smb135x-charger.c
+++ drivers/power/smb135x-charger.c
@@ -1535,7 +1535,7 @@ static int smb135x_set_usb_chg_current(struct smb135x_chg *chip,
 		goto out;
 	}
 	if (current_ma == CURRENT_500_MA) {
-		rc = smb135x_masked_write(chip, CFG_5_REG, USB_2_3_BIT, 0);
+		rc = smb135x_masked_write(chip, CFG_5_REG, USB_2_3_BIT, USB_2_3_BIT);
 		rc |= smb135x_masked_write(chip, CMD_INPUT_LIMIT,
 				USB_100_500_AC_MASK, USB_500_VAL);
 		rc |= smb135x_path_suspend(chip, USB, CURRENT, false);
