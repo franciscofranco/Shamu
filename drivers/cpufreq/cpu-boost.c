530578c3eead12b9954af8aed5b30304c48721a6
franciscofranco <franciscofranco.1990@gmail.com>
2014-12-29 02:32:28 +0000

    cpufreq: cpu-boost: fixes for underclocking cases
    We don't want sync and input_boost to set a higher min than
    the max frequency, which can happen if the user underclocks below
    the thresholds - so fix it.
    
    Also when using load_based_syncs (which it's set to enabled on FK)
    we should be evaluating the load based on max and not the absolute
    max (cpuinfo.max_freq) because users can and will underclock and we
    want to maintain the correct formula ratio.
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -161,6 +161,12 @@ static int boost_adjust_notify(struct notifier_block *nb, unsigned long val,
 
 		min = max(b_min, ib_min);
 
+		/*
+                 * If the user underclocks we don't want to boost
+                 * min freq above max
+                 */
+		min = min(min, policy->max);
+
 		pr_debug("CPU%u policy min before boost: %u kHz\n",
 			 cpu, policy->min);
 		pr_debug("CPU%u boost min: %u kHz\n", cpu, min);
@@ -254,7 +260,7 @@ static int boost_mig_sync_thread(void *data)
 			continue;
 
 		req_freq = load_based_syncs ?
-			(dest_policy.cpuinfo.max_freq * s->task_load) / 100 :
+			(dest_policy.max * s->task_load) / 100 :
 							src_policy.cur;
 
 		if (req_freq <= dest_policy.cpuinfo.min_freq) {
