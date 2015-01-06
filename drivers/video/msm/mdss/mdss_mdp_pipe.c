f83ea3a588da05c3309e7d6ee9423fb2083ba4a2
imoseyon <imoseyon@gmail.com>
2015-01-05 20:48:28 -0800

    msm: mdss: we only care about 8084 - rev 103
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/video/msm/mdss/mdss_mdp_pipe.c
+++ drivers/video/msm/mdss/mdss_mdp_pipe.c
@@ -246,7 +246,6 @@ static inline bool is_unused_smp_allowed(void)
 
         switch (MDSS_GET_MAJOR_MINOR(mdata->mdp_rev)) {
         case MDSS_GET_MAJOR_MINOR(MDSS_MDP_HW_REV_103):
-        case MDSS_GET_MAJOR_MINOR(MDSS_MDP_HW_REV_105):
                 return true;
         default:
                 return false;
