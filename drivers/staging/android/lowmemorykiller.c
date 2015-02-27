8ccbadae0b49a589f1ba8ddc27aee1c61b23769c
Liam Mark <lmark@codeaurora.org>
2015-02-27 12:59:00 -0800

    lowmemorykiller: Don't count swap cache pages twice
    The lowmem_shrink function discounts all the swap cache pages from
    the file cache count. The zone aware code also discounts all file
    cache pages from a certain zone.  This results in some swap cache
    pages being discounted twice, which can result in the low memory
    killer being unnecessarily aggressive.
    
    Fix the low memory killer to only discount the swap cache pages
    once.
    
    Conflicts:
    	drivers/staging/android/lowmemorykiller.c
    
    Change-Id: I650bbfbf0fbbabd01d82bdb3502b57ff59c3e14f
    Signed-off-by: Liam Mark <lmark@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -144,7 +144,8 @@ void tune_lmk_zone_param(struct zonelist *zonelist, int classzone_idx,
 		za = &zall[node_idx][zone_idx];
 		za->free = zone_page_state(zone, NR_FREE_PAGES);
 		za->file = zone_page_state(zone, NR_FILE_PAGES)
-					- zone_page_state(zone, NR_SHMEM);
+					- zone_page_state(zone, NR_SHMEM)
+					- zone_page_state(zone, NR_SWAPCACHE);
 		if (zone_idx == ZONE_MOVABLE) {
 			if (!use_cma_pages) {
 				unsigned long free_cma = zone_page_state(zone,
