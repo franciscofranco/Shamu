ce43ebde27583fdcaf84c3328a51cd110e93b82c
Vojtech Bocek <vbocek@gmail.com>
2015-03-05 16:51:25 +0100

    Implement kexec-hardboot
    "Allows hard booting (i.e., with a full hardware reboot) to a kernel
    previously loaded in memory by kexec.  This works around the problem of
    soft-booted kernel hangs due to improper device shutdown and/or
    reinitialization."
    More info in /arch/arm/Kconfig.
    
    Original author: Mike Kasick <mike@kasick.org>
    
    Vojtech Bocek <vbocek@gmail.com>:
      I've ported it to shamu, it is based of my hammerhead, mako, flo
      and grouper ports, which are based of Asus TF201 patch ported
      by Jens Andersen <jens.andersen@gmail.com>
    
      Shamu is rather similar to hammerhead - it uses device tree binary instead
      of atags. It is also rather picky - its bootloader erases first 128MB of RAM
      on reboot, so following hacks had to be applied:
      * Copy dtb to location passed in r2 before jumping to kexecd' kernel,
        the kernel would just freeze when I left dtb above 128MB of RAM.
      * Copy kexecd' kernel to location in the first 128MB before jumping to it,
        the decompressor operates very slowly if the kernel is above the cached
        area, copying it lower and decompressing it afterwards is much faster.
    
      The guest kernel does not require any modifications, kexec-hardboot
      is equivalent to standard kexec in this aspect.
    
    ayysir <dresadd09691@gmail.com>:
      kexec: use mem_text_write_kernel_word to set reboot_code_buffer args
      in order to avoid protection faults (writes to read-only
      kernel memory) when CONFIG_STRICT_MEMORY_RWX is enabled.
    
    Change-Id: Ia701eeaff292df5e344c9312edb61e43209c4b1b
    Signed-off-by: Vojtech Bocek <vbocek@gmail.com>
    Signed-off-by: flar2 <asegaert@gmail.com>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- include/uapi/linux/kexec.h
+++ include/uapi/linux/kexec.h
@@ -13,6 +13,10 @@
 #define KEXEC_PRESERVE_CONTEXT	0x00000002
 #define KEXEC_ARCH_MASK		0xffff0000
 
+#ifdef CONFIG_KEXEC_HARDBOOT
+#define KEXEC_HARDBOOT     0x00000004
+#endif
+
 /* These values match the ELF architecture values.
  * Unless there is a good reason that should continue to be the case.
  */
