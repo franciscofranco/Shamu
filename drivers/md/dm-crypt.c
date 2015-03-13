1981ab0ab4617f88d38849d9cab7e50d16b26637
franciscofranco <franciscofranco.1990@gmail.com>
2015-03-13 01:16:02 +0000

    crypto: revert this patchset, they were causing sequential read performance regressions
    Revert "crypto: only boost the priority for reads and increase max n of threads for wq"
    
    This reverts commit cc68815872e4ce8a5334d6afbeece04e534a3391.
    
    Revert "crypto: write thread was consuming too many cpu cycles for too long, kill its higher priority with fire. Also decrease the WQ nice level to -6, which leaves it below the GPU touch boost WQ one level."
    
    This reverts commit aa6708b46c71d36838bd94da8cb2bdff7bfc2343.
    
    Revert "crypto: convert the NICE value to PRIO so effectively decrease the previous kthread priority to something a bit more conservative"
    
    This reverts commit fd2460b2cff3c25e49489ac770837417db587b92.
    
    Revert "crypto: increase write thread and kcryptod wq prio"
    
    This reverts commit 02585071979ce0a9092993bec0f3cda65f151e7f.

--- drivers/md/dm-crypt.c
+++ drivers/md/dm-crypt.c
@@ -30,8 +30,6 @@
 
 #include <linux/device-mapper.h>
 
-#include "../../kernel/sched/sched.h"
-
 #define DM_MSG_PREFIX "crypt"
 
 /*
@@ -1203,20 +1201,13 @@ static void kcryptd_async_done(struct crypto_async_request *async_req,
 		kcryptd_crypt_write_io_submit(io);
 }
 
-/**
- * Very high priority. Max is -20 but we would be mad to boost it that high
- * Needs testing to see if this impacts user experience
- */
-static int _kcryptd_nice = -12;
-
 static void kcryptd_crypt(struct work_struct *work)
 {
 	struct dm_crypt_io *io = container_of(work, struct dm_crypt_io, work);
 
-	if (bio_data_dir(io->base_bio) == READ) {
-		set_user_nice(current, _kcryptd_nice);
+	if (bio_data_dir(io->base_bio) == READ)
 		kcryptd_crypt_read_convert(io);
-	} else
+	else
 		kcryptd_crypt_write_convert(io);
 }
 
@@ -1657,7 +1648,7 @@ static int crypt_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 
 	cc->crypt_queue = alloc_workqueue("kcryptd",
 					  WQ_CPU_INTENSIVE | WQ_MEM_RECLAIM |
-					  WQ_UNBOUND, num_possible_cpus() * 8);
+					  WQ_UNBOUND, num_online_cpus());
 	if (!cc->crypt_queue) {
 		ti->error = "Couldn't create kcryptd queue";
 		goto bad;
