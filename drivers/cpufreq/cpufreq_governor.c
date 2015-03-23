9f5692bcae7903458f1469f613a86c27497e5975
franciscofranco <franciscofranco.1990@gmail.com>
2015-03-23 16:49:00 +0000

    cpufreq: governor: do not tear down sysfs nodes (cpu going offline is enough for this to happen) to prevent shit hitting the fan with selinux and file permissions
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpufreq_governor.c
+++ drivers/cpufreq/cpufreq_governor.c
@@ -290,7 +290,7 @@ int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 
 		rc = sysfs_create_group(get_governor_parent_kobj(policy),
 				get_sysfs_attr(dbs_data));
-		if (rc) {
+		if (rc && rc != -EEXIST) {
 			cdata->exit(dbs_data);
 			kfree(dbs_data);
 			return rc;
@@ -323,9 +323,6 @@ int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 		return 0;
 	case CPUFREQ_GOV_POLICY_EXIT:
 		if (!--dbs_data->usage_count) {
-			sysfs_remove_group(get_governor_parent_kobj(policy),
-					get_sysfs_attr(dbs_data));
-
 			if (!have_governor_per_policy())
 				cpufreq_put_global_kobject();
 
