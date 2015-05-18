f7c43fd81c21ceae438e6b702ae6e4fa62248cfd
franciscofranco <franciscofranco.1990@gmail.com>
2015-05-18 17:00:21 +0000

    Revert "AIO: Don't plug the I/O queue in do_io_submit()"
    This reverts commit 993e41735ec5820ca6bedcd9d88341f9fa3ae23c.

--- fs/aio.c
+++ fs/aio.c
@@ -1152,6 +1152,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 	struct kioctx *ctx;
 	long ret = 0;
 	int i = 0;
+	struct blk_plug plug;
 
 	if (unlikely(nr < 0))
 		return -EINVAL;
@@ -1167,6 +1168,8 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		pr_debug("EINVAL: invalid context id\n");
 		return -EINVAL;
 	}
+
+	blk_start_plug(&plug);
 
 	/*
 	 * AKPM: should this return a partial result if some of the IOs were
@@ -1190,6 +1193,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		if (ret)
 			break;
 	}
+	blk_finish_plug(&plug);
 
 	put_ioctx(ctx);
 	return i ? i : ret;
