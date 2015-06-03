56d559999b472961c948bc1037295c963a1ab219
Hong-Mei Li <a21834@motorola.com>
2015-06-03 14:28:48 -0700

    IKSWL-18412 kernel: avoid adding non-thread-group task to LMK rbtree
    To maintain the task adj RB tree, we add a task to the RB tree when fork,
    and delete it when exit. The place is exactly the same as the linear
    p->tasks list, only when the task is thread_group_leader.
    
    But to handle the oom_score_adj change case, which did not check the
    thread_group_leader, we may del/add a non-leader task to the RB tree.
    Finally leave the task in the RB tree, since we would not really delete
    a non-leader task from the tree. The orphan task would finally be freed,
    and cause later use-after-free panic when accessing RB tree.
    
    Solution:
    Move the rbtree adj_node to signal_struct, which is shared between
    task and all threads. This can make sure we only add one node for
    a thread group.
    
    Change-Id: I1e8dfe490656408863b3726c7bc9e4ee6dc5abc1
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Reviewed-on: http://gerrit.mot.com/754224
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1103,6 +1103,9 @@ static int copy_signal(unsigned long clone_flags, struct task_struct *tsk)
 	sig->oom_score_adj = current->signal->oom_score_adj;
 	sig->oom_score_adj_min = current->signal->oom_score_adj_min;
 
+#ifdef CONFIG_ANDROID_LMK_ADJ_RBTREE
+	RB_CLEAR_NODE(&sig->adj_node);
+#endif
 	sig->has_child_subreaper = current->signal->has_child_subreaper ||
 				   current->signal->is_child_subreaper;
 
