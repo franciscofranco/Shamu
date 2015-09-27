db60c1c4660170cc036b434920b464295c51f8d7
franciscofranco <franciscofranco.1990@gmail.com>
2015-09-27 18:52:16 +0000

    Revert "mdss: we can probably use interruptible thread on this code path"
    Probably not such a good idea after all.
    
    This reverts commit 31bd3316103c9786934d22bfa73abafe36968440.

--- drivers/video/msm/mdss/mdss_dsi_host.c
+++ drivers/video/msm/mdss/mdss_dsi_host.c
@@ -1714,15 +1714,7 @@ static int dsi_event_thread(void *data)
 	spin_lock_init(&ev->event_lock);
 
 	while (1) {
-		ret = wait_event_interruptible(ev->event_q,
-			(ev->event_pndx != ev->event_gndx) ||
-			kthread_should_stop());
-
-		if (ret) {
-			pr_debug("%s: interrupted", __func__);
-			continue;
-		}
-
+		wait_event(ev->event_q, (ev->event_pndx != ev->event_gndx));
 		spin_lock_irqsave(&ev->event_lock, flag);
 		evq = &ev->todo_list[ev->event_gndx++];
 		todo = evq->todo;
