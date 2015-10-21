8df12a62464016bddb054df48a3ee7a55f0e6197
franciscofranco <franciscofranco.1990@gmail.com>
2015-10-21 15:26:03 +0000

    Don't reset keys pressed at suspend time
    Adapted for Shamu, from here: https://github.com/atl4ntis/kernel_msm/commit/f28b6c17a801d023fc607bfd17902ddb565dc047
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -1657,19 +1657,8 @@ void input_reset_device(struct input_dev *dev)
 {
 	mutex_lock(&dev->mutex);
 
-	if (dev->users) {
+	if (dev->users)
 		input_dev_toggle(dev, true);
-
-		/*
-		 * Keys that have been pressed at suspend time are unlikely
-		 * to be still pressed when we resume.
-		 */
-		if (!test_bit(INPUT_PROP_NO_DUMMY_RELEASE, dev->propbit)) {
-			spin_lock_irq(&dev->event_lock);
-			input_dev_release_keys(dev);
-			spin_unlock_irq(&dev->event_lock);
-		}
-	}
 
 	mutex_unlock(&dev->mutex);
 }
