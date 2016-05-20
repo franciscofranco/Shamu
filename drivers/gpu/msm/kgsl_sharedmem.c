504a64cd6fbb9bd09009e88729ce3a82c8dff318
Rajesh Kemisetti <rajeshk@codeaurora.org>
2016-05-20 14:56:40 +0530

    msm: kgsl: Add missing check for alloc size
    In _kgsl_sharedmem_page_alloc(), check for boundary limits
    of requested alloc size before honoring.
    
    CRs-Fixed: 988860
    Change-Id: I8b9e225e515a0f31593df6f4cad253236475d0ae
    Signed-off-by: Rajesh Kemisetti <rajeshk@codeaurora.org>
    Signed-off-by: Santhosh Punugu <spunug@codeaurora.org>
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -604,6 +604,10 @@ _kgsl_sharedmem_page_alloc(struct kgsl_memdesc *memdesc,
 	if (size == 0 || size > UINT_MAX)
 		return -EINVAL;
 
+	size = PAGE_ALIGN(size);
+	if (size == 0 || size > UINT_MAX)
+		return -EINVAL;
+
 	align = (memdesc->flags & KGSL_MEMALIGN_MASK) >> KGSL_MEMALIGN_SHIFT;
 
 	page_size = (align >= ilog2(SZ_64K) && size >= SZ_64K)
