71c88d80799f3f014b92618e1e078fd84d61c633
Tim Murray <timmurray@google.com>
2016-07-12 16:01:21 -0700

    mdss: move to a kthread for vsync_retire_work_handler
    vsync_retire_work_handler is in the critical display path and should
    never be delayed because of other non-FIFO work.
    
    bug 30115868
    
    Change-Id: Ic97744a81198ff303f9c87e1cf7c2468b4356a25

--- drivers/video/msm/mdss/mdss_mdp.h
+++ drivers/video/msm/mdss/mdss_mdp.h
@@ -21,6 +21,7 @@
 #include <linux/notifier.h>
 #include <linux/irqreturn.h>
 #include <linux/kref.h>
+#include <linux/kthread.h>
 
 #include "mdss.h"
 #include "mdss_mdp_hwio.h"
@@ -485,9 +486,12 @@ struct mdss_overlay_private {
 
 	struct sw_sync_timeline *vsync_timeline;
 	struct mdss_mdp_vsync_handler vsync_retire_handler;
-	struct work_struct retire_work;
 	int retire_cnt;
 	bool kickoff_released;
+
+	struct kthread_worker worker;
+	struct kthread_work vsync_work;
+	struct task_struct *thread;
 };
 
 struct mdss_mdp_commit_cb {
