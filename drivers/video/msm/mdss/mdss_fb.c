a84159cf12594ce3e5708125b58034d26ca5c8c1
Francisco Franco <franciscofranco.1990@gmail.com>
2016-09-13 03:35:49 +0000

    drivers: mdss: disable backlight dimmer by default
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -65,7 +65,7 @@
 				out = (((v) - 2) * 255 / 250);\
 				} while (0)
 
-bool backlight_dimmer = true;
+bool backlight_dimmer = false;
 module_param(backlight_dimmer, bool, 0664);
 
 #define MAX_FBI_LIST 32
