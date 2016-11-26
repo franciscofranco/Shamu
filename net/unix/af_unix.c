767756a9cd8dc01c82ee10a0be3e9b3102b75818
Francisco Franco <franciscofranco.1990@gmail.com>
2016-11-26 15:43:16 +0000

    af_unix: fix tree version mismerge of b006ad2
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -2131,7 +2131,7 @@ again:
 			if (signal_pending(current)
 			    ||  mutex_lock_interruptible(&u->readlock)) {
 				err = sock_intr_errno(timeo);
-				scm_destroy(&scm);
+				scm_destroy(siocb->scm);
 				goto out;
 			}
 
