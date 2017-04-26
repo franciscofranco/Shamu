f5522646bac89ad4d6dc3bf31f54ef049d5719c0
Nathan Chancellor <natechancellor@gmail.com>
2017-04-25 19:29:47 -0700

    [readdir] convert sdcardfs
    Taken from: https://android.googlesource.com/kernel/msm/+/5f2ddb1235abf9a33b2563bed07528277893c4f1%5E%21/#F2
    
    Change-Id: Iefdb131195e56acb1d661d6aa22848a74474a857
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- fs/sdcardfs/file.c
+++ fs/sdcardfs/file.c
@@ -82,7 +82,7 @@ static ssize_t sdcardfs_write(struct file *file, const char __user *buf,
 	return err;
 }
 
-static int sdcardfs_readdir(struct file *file, void *dirent, filldir_t filldir)
+static int sdcardfs_readdir(struct file *file, struct dir_context *ctx)
 {
 	int err = 0;
 	struct file *lower_file = NULL;
@@ -91,7 +91,7 @@ static int sdcardfs_readdir(struct file *file, void *dirent, filldir_t filldir)
 	lower_file = sdcardfs_lower_file(file);
 
 	lower_file->f_pos = file->f_pos;
-	err =  vfs_readdir(lower_file, filldir, dirent);
+	err =  iterate_dir(lower_file, ctx);
 	file->f_pos = lower_file->f_pos;
 	if (err >= 0)		/* copy the atime */
 		fsstack_copy_attr_atime(dentry->d_inode,
@@ -360,7 +360,7 @@ const struct file_operations sdcardfs_main_fops = {
 const struct file_operations sdcardfs_dir_fops = {
 	.llseek		= sdcardfs_file_llseek,
 	.read		= generic_read_dir,
-	.readdir	= sdcardfs_readdir,
+	.iterate	= sdcardfs_readdir,
 	.unlocked_ioctl	= sdcardfs_unlocked_ioctl,
 #ifdef CONFIG_COMPAT
 	.compat_ioctl	= sdcardfs_compat_ioctl,
