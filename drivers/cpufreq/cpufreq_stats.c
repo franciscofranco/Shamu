198de65bfb49a5ee13d7bae042bf24de6bcda622
Andres Oportus <andresoportus@google.com>
2017-05-11 08:57:39 -0700

    ANDROID: Fix cpufreq stats table creation
    cpufreq stats does not correctly supports multiple cpus per profile.
    For instance Marlin/Sailfish per cpu stats struct does not get created
    for all cpus (only one per policy).  This change does not provide full
    support for multiple cpus per profile but allows stats creation per
    cpu to allow b/34133340 to be completed.
    
    Bug: 38244231
    Bug: 34133340
    Test: Boot Sailfish
    
    Signed-off-by: Andres Oportus <andresoportus@google.com>
    Change-Id: I72ea548a199f57ed841618b08b9c41e99b493376

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -573,12 +573,11 @@ static void cpufreq_powerstats_free(void)
 }
 
 static int __cpufreq_stats_create_table(struct cpufreq_policy *policy,
-		struct cpufreq_frequency_table *table, int count)
+	int cpu, struct cpufreq_frequency_table *table, int count)
 {
 	unsigned int i, j, ret = 0;
 	struct cpufreq_stats *stat;
 	unsigned int alloc_size;
-	unsigned int cpu = policy->cpu;
 	if (per_cpu(cpufreq_stats_table, cpu))
 		return -EBUSY;
 	stat = kzalloc(sizeof(*stat), GFP_KERNEL);
@@ -586,8 +585,6 @@ static int __cpufreq_stats_create_table(struct cpufreq_policy *policy,
 		return -ENOMEM;
 
 	ret = sysfs_create_group(&policy->kobj, &stats_attr_group);
-	if (ret)
-		goto error_out;
 
 	stat->cpu = cpu;
 	per_cpu(cpufreq_stats_table, cpu) = stat;
@@ -625,7 +622,6 @@ static int __cpufreq_stats_create_table(struct cpufreq_policy *policy,
 	return 0;
 error_alloc:
 	sysfs_remove_group(&policy->kobj, &stats_attr_group);
-error_out:
 	kfree(stat);
 	per_cpu(cpufreq_stats_table, cpu) = NULL;
 	return ret;
@@ -827,7 +823,7 @@ static int cpufreq_stat_notifier_policy(struct notifier_block *nb,
 		cpufreq_powerstats_create(cpu, table, count);
 
 	if (val == CPUFREQ_CREATE_POLICY)
-		ret = __cpufreq_stats_create_table(policy, table, count);
+		ret = __cpufreq_stats_create_table(policy, cpu, table, count);
 	else if (val == CPUFREQ_REMOVE_POLICY)
 		__cpufreq_stats_free_table(policy);
 
@@ -863,7 +859,7 @@ static void cpufreq_stats_create_table(unsigned int cpu)
 		if (!per_cpu(cpufreq_power_stats, cpu))
 			cpufreq_powerstats_create(cpu, table, count);
 
-		__cpufreq_stats_create_table(policy, table, count);
+		__cpufreq_stats_create_table(policy, cpu, table, count);
 	}
 	cpufreq_cpu_put(policy);
 }
