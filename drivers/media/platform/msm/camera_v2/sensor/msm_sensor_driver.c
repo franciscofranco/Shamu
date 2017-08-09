090aa04e760e7fae9fb550570a3f9564fc192ae2
Haibin Liu <haibinl@codeaurora.org>
2017-08-09 16:26:41 +0800

    msm: sensor: Fix crash when ioctl VIDIOC_MSM_SENSOR_INIT_CFG
    Issue:
    the invalid slave_info is used by msm_sensor_driver_probe.
    This cause crash when ioctl VIDIOC_MSM_SENSOR_INIT_CFG repeatedly.
    
    Fix:
    1) avoid the same msm_sd_subdev added into the ordered_sd_list.
    2) enlarge the buffer size for i2c addr and data.
    
    Bug: 36492827
    Change-Id: Idffcd3b82b9590dbfdcaf14b80668cc894178f54
    Signed-off-by: Haibin Liu <haibinl@codeaurora.org>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2015,2017 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -106,7 +106,11 @@ static int32_t msm_sensor_driver_create_i2c_v4l_subdev
 	s_ctrl->msm_sd.sd.entity.name =	s_ctrl->msm_sd.sd.name;
 	s_ctrl->sensordata->sensor_info->session_id = session_id;
 	s_ctrl->msm_sd.close_seq = MSM_SD_CLOSE_2ND_CATEGORY | 0x3;
-	msm_sd_register(&s_ctrl->msm_sd);
+	rc = msm_sd_register(&s_ctrl->msm_sd);
+	if (rc < 0) {
+		pr_err("failed: msm_sd_register rc %d", rc);
+		return rc;
+	}
 	CDBG("%s:%d\n", __func__, __LINE__);
 	return rc;
 }
@@ -136,7 +140,11 @@ static int32_t msm_sensor_driver_create_v4l_subdev
 	s_ctrl->msm_sd.sd.entity.group_id = MSM_CAMERA_SUBDEV_SENSOR;
 	s_ctrl->msm_sd.sd.entity.name = s_ctrl->msm_sd.sd.name;
 	s_ctrl->msm_sd.close_seq = MSM_SD_CLOSE_2ND_CATEGORY | 0x3;
-	msm_sd_register(&s_ctrl->msm_sd);
+	rc = msm_sd_register(&s_ctrl->msm_sd);
+	if (rc < 0) {
+		pr_err("failed: msm_sd_register rc %d", rc);
+		return rc;
+	}
 	return rc;
 }
 
@@ -821,12 +829,6 @@ int32_t msm_sensor_driver_probe(void *setting)
 	}
 
 	/*
-	  Set probe succeeded flag to 1 so that no other camera shall
-	 * probed on this slot
-	 */
-	s_ctrl->is_probe_succeed = 1;
-
-	/*
 	 * Update the subdevice id of flash-src based on availability in kernel.
 	 */
 	if (slave_info->is_flash_supported == 0) {
@@ -870,6 +872,11 @@ int32_t msm_sensor_driver_probe(void *setting)
 		sensor_mount_angle / 90) << 8);
 	s_ctrl->msm_sd.sd.entity.flags = mount_pos | MEDIA_ENT_FL_DEFAULT;
 
+	/*
+	 * Set probe succeeded flag to 1 so that no other camera shall
+	 * probed on this slot
+	 */
+	s_ctrl->is_probe_succeed = 1;
 	return rc;
 
 CAMERA_POWER_DOWN:
