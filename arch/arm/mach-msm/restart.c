0741cb9f76eeba545209a48788105216f992df88
Wei Li <sirius.liwei@huawei.com>
2017-12-06 15:03:05 +0100

    apq8084: remove 'reboot edl' interface for security.
    BUG: 63100473
    
    Change-Id: If6cb5ce50524d34b30f09a22eeb76d2cd3a0e834
    (cherry picked from commit 1e265940c3403eae5e137d44f30b37cb741b6595)

--- arch/arm/mach-msm/restart.c
+++ arch/arm/mach-msm/restart.c
@@ -101,6 +101,7 @@ static bool get_dload_mode(void)
 	return dload_mode_enabled;
 }
 
+#if 0
 static void enable_emergency_dload_mode(void)
 {
 	if (emergency_dload_mode_addr) {
@@ -119,6 +120,7 @@ static void enable_emergency_dload_mode(void)
 		mb();
 	}
 }
+#endif
 
 static int dload_set(const char *val, struct kernel_param *kp)
 {
@@ -143,10 +145,12 @@ static int dload_set(const char *val, struct kernel_param *kp)
 #else
 #define set_dload_mode(x) do {} while (0)
 
+#if 0
 static void enable_emergency_dload_mode(void)
 {
 	printk(KERN_ERR "dload mode is not enabled on target\n");
 }
+#endif
 
 static bool get_dload_mode(void)
 {
@@ -246,8 +250,10 @@ static void msm_restart_prepare(const char *cmd)
 			unsigned long code;
 			code = simple_strtoul(cmd + 4, NULL, 16) & 0xff;
 			__raw_writel(0x6f656d00 | code, restart_reason);
+#if 0
 		} else if (!strncmp(cmd, "edl", 3)) {
 			enable_emergency_dload_mode();
+#endif
 		} else {
 			__raw_writel(0x77665501, restart_reason);
 		}
