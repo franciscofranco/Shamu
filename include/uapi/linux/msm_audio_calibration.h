445daec9560be2d4c681f98792df24d8b4bee696
Christian Oder <myself5@carbonrom.org>
2017-12-16 18:14:33 -0500

    include: Include dummy MSM Audio Calibration header
    * This needs to be included in apq8084/msm8974 kernel's exported headers to build the msm audio hal as of oreo-mr1.
    * Demon000 deserves credit for the initial idea behind this commit, as he pointed out which structs were used.
    
    Change-Id: I55d84676b14642338c9c353f1164938e021547bb
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

---
+++ include/uapi/linux/msm_audio_calibration.h
@@ -0,0 +1,8 @@
+#ifndef _UAPI_MSM_AUDIO_CALIBRATION_H
+#define _UAPI_MSM_AUDIO_CALIBRATION_H
+
+struct audio_cal_info_metainfo {
+    uint32_t nKey;
+};
+
+#endif /* _UAPI_MSM_AUDIO_CALIBRATION_H */
